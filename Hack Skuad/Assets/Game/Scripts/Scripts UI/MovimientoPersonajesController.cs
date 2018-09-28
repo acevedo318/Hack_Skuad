4db1536bec85b3006725ea5ca5c35c9268d72024
MrDooF07 <43276687+MrDooF07@users.noreply.github.com>
2018-09-28 00:01:19 -0500

    Interfaz de usuario
    Interfaz de usuario casi lista, falta pulirla un poco más

---
+++ Hack Skuad/Assets/Game/Scripts/Scripts UI/MovimientoPersonajesController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimientoPersonajesController : MonoBehaviour
+{
+
+    [SerializeField]
+    private Animator animadorMovimientoPersonajes; // Llamado al animator que controla el movimiento de los personajes en la UI
+
+    [SerializeField]
+    private GameObject panelSeleccionRol; // Llamado al panel de Seleccion Rol, donde se deben ubicar los personajes
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UbicacionPersonajes();
+    }
+
+    //Método para ubicar a los personajes en sus respectivos Toggles, con el fin de ayudar al usuario a identificarlos mejor
+    public void UbicacionPersonajes()
+    {
+
+        if (panelSeleccionRol.activeSelf.Equals(true)) // Se evalua si el panel de seleccion de rol esta activo, si lo está, se setea un disparador para dar paso a la transición
+        {
+
+            animadorMovimientoPersonajes.SetTrigger("DisparadorMovimiento");
+
+        }
+
+    }
+}
---
+++ Hack Skuad/Assets/Game/Scripts/Scripts UI/SelectRolController.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SelectRolController : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject botonIniciarPartida; // Botón para iniciar el juego al momento de chequear todos los toggles.
+
+    void Start()
+    {
+        botonIniciarPartida.SetActive(false); // Al iniciar el menú SeleccionarRol, el botón está deshabilitado.
+    }
+
+    bool checkInteger = false; // Variable booleana para chequear el estado del Toggle Integer
+    bool checkString = false; // Variable booleana para chequear el estado del Toggle String
+    bool checkBoolean = false; // Variable booleana para chequear el estado del Toggle Boolean
+    bool checkDouble = false; // Variable booleana para chequear el estado del Toggle Double
+    bool checkAntivirus = false; // Variable booleana para chequear el estado del Toggle Antivirus
+
+    // Método para cambiar el estado de la variable booleana checkInteger a "true" y se verifica el método "Validación"
+    public void VerificacionInteger()
+    {
+        checkInteger = !checkInteger;
+        ValidacionTodosToggles();
+    }
+
+    // Método para cambiar el estado de la variable booleana checkString a "true" y se verifica el método "Validación"
+    public void ValidacionString()
+    {
+        checkString = !checkString;
+        ValidacionTodosToggles();
+    }
+
+    // Método para cambiar el estado de la variable booleana checkBoolean a "true" y se verifica el método "Validación"
+    public void ValidacionBoolean()
+    {
+        checkBoolean = !checkBoolean;
+        ValidacionTodosToggles();
+    }
+
+    // Método para cambiar el estado de la variable booleana checkDouble a "true" y se verifica el método "Validación"
+    public void ValidacionDouble()
+    {
+        checkDouble = !checkDouble;
+        ValidacionTodosToggles();
+    }
+
+    // Método para cambiar el estado de la variable booleana checkAntivirus a "true" y se verifica el método "Validación"
+    public void ValidacionAntivirus()
+    {
+        checkAntivirus = !checkAntivirus;
+        ValidacionTodosToggles();
+    }
+
+    // Método para verificar el estado de todas las variables booleanas, si son todas verdaderas, el botón "Iniciar" se activa, de lo contrario no
+    private void ValidacionTodosToggles()
+    {
+        if (checkBoolean && checkDouble && checkInteger && checkString && checkAntivirus)
+        {
+            botonIniciarPartida.SetActive(true);
+        }
+        else
+        {
+            botonIniciarPartida.SetActive(false);
+        }
+    }
+
+    // Método para cambiar de la escena actual a la del juego principal
+    public void CambioEscena(string nombreEscena)
+    {
+        SceneManager.LoadScene(nombreEscena);
+    }
+}
