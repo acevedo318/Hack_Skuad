31179ef365dbc3ff9744421759cdd2bab6cd25fe
MrDooF07 <43276687+MrDooF07@users.noreply.github.com>
2018-10-13 23:51:51 -0500

    Dados ubicacion antivirus

--- Hack Skuad/Assets/Game/Scripts/Scripts Game/DadoAntivirus.cs
+++ Hack Skuad/Assets/Game/Scripts/Scripts Game/DadoAntivirus.cs
@@ -1,16 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class DadoAntivirus : MonoBehaviour {
+public class DadoAntivirus : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
+    [SerializeField]
+    private Sprite[] ladosDado;
+
+    [SerializeField]
+    private Image imagenDado;
+
+    int i;
+
+    // Use this for initialization
+    void Start ()
+    {
+        i = 0;
+        //imagenDado = GameObject.Find("DadoFlechaAntivirus").GetComponent<Image>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    public void CambiarLadoDado()
+    {
+        i++;
+        while (i <= 3) 
+        {
+            imagenDado.sprite = ladosDado[i];          
+        }
+        if (i == 4) i = 0;
+
+    }
 }
